e49be60c7521c8158f0d9dbf1bd3de6690138be5
Thomas POYETON <poyetont@gmail.com>
2024-10-08 15:47:45 +0200

    Update example to get version

--- examples/get_version.ts
+++ examples/get_version.ts
@@ -3,6 +3,6 @@ import { Client } from '../src'
 const client = new Client(process.env.MISP_URL!, process.env.MISP_API_KEY!);
 
 (async () => {
-    const version = await client.servers.pull('1', 'full');
+    const version = await client.servers.version();
     console.log(version);
 })();
