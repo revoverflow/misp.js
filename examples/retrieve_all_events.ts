af50f808183a0e2dd83c7200a7374e76ce2a62af
Thomas POYETON <poyetont@gmail.com>
2024-10-08 14:51:08 +0200

    Switching to tsup for building and publishing new version

--- examples/retrieve_all_events.ts
+++ examples/retrieve_all_events.ts
@@ -2,6 +2,7 @@ import { Client } from '../src'
 
 const client = new Client(process.env.MISP_URL!, process.env.MISP_API_KEY!);
 
-const events = await client.events.list();
-
-console.log(JSON.stringify(events, null, 2));
+(async () => {
+    const events = await client.events.list();
+    console.log(JSON.stringify(events, null, 2));
+})();
